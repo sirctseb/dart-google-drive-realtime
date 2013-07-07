22de54264b2e1abab2e58085438b1e633d344669
Christopher Best <bestchris@gmail.com>
2013-07-07 16:48:44 -0400

    added most events to local string

--- lib/src/LocalModelString.dart
+++ lib/src/LocalModelString.dart
@@ -17,28 +17,35 @@ part of realtime_data_model;
 class LocalModelString extends LocalModelObject implements rt.CollaborativeString {
 
   // TODO need local events
-  StreamController<rt.TextInsertedEvent> _onTextInserted;
-  StreamController<rt.TextDeletedEvent> _onTextDeleted;
+  StreamController<LocalTextInsertedEvent> _onTextInserted
+    = new StreamController<LocalTextInsertedEvent>.broadcast(sync: true);
+  StreamController<LocalTextDeletedEvent> _onTextDeleted
+    = new StreamController<LocalTextDeletedEvent>.broadcast(sync: true);
 
   int get length => _string.length;
 
   void append(String text) {
-    // TODO event
     _string = "${_string}text";
+    // add event to stream
+    // TODO check index value computation
+    _onTextInserted.add(new LocalTextInsertedEvent._(_string.length - text.length, text));
   }
   String get text => _string;
   void insertString(int index, String text) {
-    // TODO event
     // TODO make sure end is exclusive in substring
     _string = "${_string.substring(0, index)}text${_string.substring(index)}";
+    _onTextInserted.add(new LocalTextInsertedEvent._(index, text));
   }
   // TODO implement references
   IndexReference registerReference(int index, bool canBeDeleted) => null;
   void removeRange(int startIndex, int endIndex) {
-    // TODO event
+    // get removed text for event
+    var removed = _string.substring(startIndex, endIndex);
     // TODO make sure end is exclusive in substring
     // TODO test edge case where endIndex is last index. I think this will crash
     _string = "${_string.substring(0, startIndex)}${_string.substring(endIndex)}";
+    // add event to stream
+    _onTextDeleted.add(new LocalTextDeletedEvent._(startIndex, removed));
   }
   void set text(String text) {
     // TODO compute edit
@@ -47,8 +54,8 @@ class LocalModelString extends LocalModelObject implements rt.CollaborativeStrin
   }
 
   // TODO need local events
-  Stream<rt.TextInsertedEvent> get onTextInserted => _onTextInserted.stream;
-  Stream<rt.TextDeletedEvent> get onTextDeleted => _onTextDeleted.stream;
+  Stream<LocalTextInsertedEvent> get onTextInserted => _onTextInserted.stream;
+  Stream<LocalTextDeletedEvent> get onTextDeleted => _onTextDeleted.stream;
   
   // current string value
   String _string;
