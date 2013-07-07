1d95f90a4a9925fa6181fed9d4980f058dcbaa28
Christopher Best <bestchris@gmail.com>
2013-07-07 14:20:37 -0400

    implemented events for local list

--- lib/src/LocalModelList.dart
+++ lib/src/LocalModelList.dart
@@ -19,25 +19,36 @@ class LocalModelList<E> extends ModelList<E> {
   E operator[](int index) => _list[index];
 
   void operator[]=(int index, E value) {
-    // TODO value set event
+    if (index < 0 || index >= length) throw new RangeError.value(index);
+    // get old value
+    var oldValue = _list[index];
     _list[index] = value;
+    // add event to stream
+    // TODO might still be worth checking for listener to save on these
+    _onValuesSet.add(new LocalValuesSetEvent._(index, [value], [oldValue]));
   }
 
   List<E> asArray() => _list;
 
   void clear() {
-    // TODO values removed event
+    // clone list
+    var list = new List.from(_list);
     _list.clear();
+    // add event to stream
+    _onValuesRemoved.add(new LocalValuesRemovedEvent._(0,list));
   }
 
   void insert(int index, E value) {
-    // TODO values added event
     _list.insert(index, value);
+    // add event to stream
+    _onValuesAdded.add(new LocalValuesAddedEvent._(index, [value]));
   }
 
   void insertAll(int index, List<E> values) {
-    // TODO values added event
     _list.insertAll(index, values);
+    // add event to stream
+    // TODO clone values?
+    _onValuesAdded.add(new LocalValuesAddedEvent._(index, values));
   }
 
   // TODO anything with comparator?
@@ -54,14 +65,18 @@ class LocalModelList<E> extends ModelList<E> {
   Stream<rt.ValuesSetEvent> get onValuesSet => _onValuesSet.stream;
 
   int push(E value) {
-    // TODO values added event
     _list.add(value);
+    // add event to stream
+    // TODO make sure this is the index provided when inserting at the end
+    _onValuesAdded.add(new LocalValuesAddedEvent._(_list.length, [value]));
     return _list.length;
   }
 
   void pushAll(List<E> values) {
-    // TODO values added event
     _list.addAll(values);
+    // add event to stream
+    // TODO make sure this is the index provided when inserting at the end
+    _onValuesAdded.add(new LocalValuesAddedEvent._(_list.length - values.length + 1, values));
   }
 
   IndexReference registerReference(int index, bool canBeDeleted) {
@@ -70,23 +85,39 @@ class LocalModelList<E> extends ModelList<E> {
 
   // TODO this is an actual conflict with the List interface and would make it harder to implement it
   void remove(int index) {
-    // TODO values removed event
-    _list.removeAt(index);
+    var removed = _list.removeAt(index);
+    // add event to stream
+    _onValuesRemoved.add(new LocalValuesRemovedEvent._(index, [removed]));
   }
 
   void removeRange(int startIndex, int endIndex) {
-    // TODO values removed event
+    // get range to return it
+    var removed = _list.sublist(startIndex, endIndex);
+    // remove from list
     _list.removeRange(startIndex, endIndex);
+    // add event to stream
+    _onValuesRemoved.add(new LocalValuesRemovedEvent._(startIndex, removed));
   }
 
   bool removeValue(E value) {
-    // TODO values removed event
+    // get index of value for event
+    int index = _list.indexOf(value);
+    // remove from list
     _list.remove(value);
+    if(index != -1) {
+      // add to stream
+      _onValuesRemoved.add(new LocalValuesRemovedEvent._(index, [value]));
+    }
   }
 
   void replaceRange(int index, List<E> values) {
-    // TODO values set event
+    // get current values for event
+    var current = _list.sublist(index, index + values.length);
+    // replace values in list
     _list.replaceRange(index, index + values.length, values);
+    // add event to stream
+    // TODO clone values?
+    _onValuesSet.add(new LocalValuesSetEvent._(index, values, current));
   }
   
   // backing field
