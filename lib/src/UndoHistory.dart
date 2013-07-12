e507c0ff942438d2243c8385998dbc0e53874144
Christopher Best <bestchris@gmail.com>
2013-07-12 13:27:10 -0400

    implemented most of undo history

--- lib/src/UndoHistory.dart
+++ lib/src/UndoHistory.dart
@@ -30,8 +30,66 @@ class UndoHistory {
   /** The list of actions in the undo history */
   List<List<LocalUndoableEvent>> _history = [];
 
-  /** The current index into the undo history.
-   * At any time, history[index-1] has been performed and history index has not
-   */
-  int _index = 0;
+  /// The current index into the undo history.
+  int _index = -1;
+
+  // Add a list of events to the current undo index
+  void _addUndoEvents(List<LocalUndoableEvent> events, {bool newSet: false}) {
+    if(newSet) {
+      _history.add([]);
+      _index++;
+    }
+    _history[_index].addAll(events);
+  }
+
+  bool _undoLatch = false;
+  bool _redoLatch = false;
+  UndoHistory(LocalModelMap root) {
+    root.onObjectChanged.listen((LocalObjectChangedEvent e) {
+      if(_undoLatch) {
+        // if undoing, add inverse of events to history
+        _addUndoEvents(e.events);
+      } else if(_undoLatch) {
+        // if redoing, add events to history
+        _addUndoEvents(e.events);
+      } else {
+        // add event to current undo set
+        _addUndoEvents(e.events, newSet: e._undoSetRoot);
+        // TODO if new undo set, truncate history after this
+      }
+    });
+  }
+
+  void undo() {
+    // set undo latch
+    _undoLatch = true;
+    // save current events
+    var current = _history[_index];
+    // put empty list in place
+    _history[_index] = [];
+    // undo events
+    current.reversed.forEach((e) => e._undo());
+    // decrement index
+    _index--;
+    // unset undo latch
+    _undoLatch = false;
+  }
+  void redo() {
+    // set redo latch
+    _redoLatch = true;
+    // increment index
+    _index++;
+    // save current events
+    var current = _history[_index];
+    // put empty list in place
+    _history[_index];
+    // redo events
+    // TODO we call undo instead of redo because the events in the list
+    // TODO are events instigated by undo calls, so they are the inverse of what
+    // TODO The original events were. this is not obvious and we should consider
+    // TODO changing it to make it clearer
+    current.reversed.forEach((e) => e._undo());
+    // uset redo latch
+    _redoLatch = false;
+  }
 }
