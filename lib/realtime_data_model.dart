8371bda37ec0161baba39cce3beac9e581832b42
Christopher Best <bestchris@gmail.com>
2013-07-31 11:17:36 -0400

    use authentication and drive api without js removed js client utils file

--- lib/realtime_data_model.dart
+++ lib/realtime_data_model.dart
@@ -22,6 +22,9 @@ import 'package:js/js.dart' as js;
 import 'package:js/js_wrapping.dart' as jsw;
 import 'package:meta/meta.dart';
 import 'package:realtime_data_model/src/local/local_realtime_data_model.dart';
+import 'package:google_oauth2_client/google_oauth2_browser.dart';
+import 'package:google_drive_v2_api/drive_v2_api_browser.dart' as driveclient;
+import 'package:google_drive_v2_api/drive_v2_api_client.dart' as driveclient;
 
 import 'src/utils.dart';
 
@@ -60,14 +63,16 @@ final realtime = js.retain(js.context['gapi']['drive']['realtime']);
 
 String get token => realtime['getToken']();
 
-Future<Document> load(String docId, [void initializerFn(Model model), void errorFn(Error error)]) {
-  final completer = new Completer();
+// load a realtime object for a file with the given id
+void load(String docId,
+  [void onFileLoaded(Document document),
+   void initializeModel(Model model),
+   void errorFn(Error error)]) {
   realtime.load(docId,
-      new js.Callback.once((js.Proxy p) => completer.complete(new Document._fromProxy(p))),
-      initializerFn == null ? null : new js.Callback.once((js.Proxy p) => initializerFn(new Model._fromProxy(p))),
-      errorFn == null ? null : new js.Callback.once((js.Proxy p) => errorFn(new Error._fromProxy(p)))
+      onFileLoaded == null ? null : new js.Callback.once((p) => onFileLoaded(new Document._fromProxy(p))),
+      initializeModel == null ? null : new js.Callback.once((p) => initializeModel(new Model._fromProxy(p))),
+      errorFn == null ? null : new js.Callback.once((p) => errorFn(new Error._fromProxy(p)))
   );
-  return completer.future;
 }
 
 
@@ -83,22 +88,88 @@ void start(Map realtimeOptions, {bool local: false}) {
     // do onFileLoaded callback
     realtimeOptions["onFileLoaded"](document);
   } else {
-    // convert callbacks for js
-    if(realtimeOptions["initializeModel"] != null) {
-      var nativeInitializeModel = realtimeOptions["initializeModel"];
-      realtimeOptions["initializeModel"] = new js.Callback.once((modelProxy) {
-        nativeInitializeModel(new Model._fromProxy(modelProxy));
+    // function to create a Drive file and load the realtime object
+    // using an existing OAuth2 object
+    var createAndLoadFile = (OAuth2 auth) {
+      // create drive api object
+      var drive = new driveclient.Drive(auth);
+      // allow it to make authorized requests (I guess. I got it from the examples)
+      drive.makeAuthRequests = true;
+      // insert a new file
+      drive.files.insert(
+        new driveclient.File.fromJson({'mimetype': 'application/vnd.google-apps.drive-sdk', 'title': 'tabasci tab'})
+      ).then((driveclient.File file) {
+          // load realtime object for the file
+          load(file.id, realtimeOptions['onFileLoaded'], realtimeOptions['initializeModel']);
+      }).catchError((obj) {
+        print('file insert failed: $obj');
       });
-    }
-    if(realtimeOptions["onFileLoaded"] != null) {
-      var nativeOnFileLoaded = realtimeOptions["onFileLoaded"];
-      realtimeOptions["onFileLoaded"] = new js.Callback.many((docProxy) {
-        nativeOnFileLoaded(new Document._fromProxy(docProxy));
-      });
-    }
-    // create loader and start
-    var realtimeLoader = new js.Proxy(js.context.rtclient.RealtimeLoader, js.map(realtimeOptions));
-    realtimeLoader.start();
+    };
+
+    // load realtime api before starting
+    js.context['gapi']['load']('drive-realtime', new js.Callback.once(() {
+
+      // TODO realtime library checks gapi.auth.getToken().access_token for authentication,
+      // so we need to do one of two things:
+      // 1. authenticate on the javascript side and let gapi.auth set itself up, or
+      // 2. authenticate on the dart side and set gapi.auth.getToken to a function
+      //    that returns an object with a valid access_token
+
+      int ofTheTwoThings = 1;
+
+      if(ofTheTwoThings == 1) {
+        // 1:
+        // load js auth client library
+        js.context['gapi']['load']('auth:client', new js.Callback.once(() {
+          // authorize on js side
+          js.context['gapi']['auth']['authorize'](js.map({
+              'client_id': '1066816720974',
+              'scope': ['https://www.googleapis.com/auth/drive.install',
+                        'https://www.googleapis.com/auth/drive.file',
+                        'openid'],
+              'immediate': true
+            }), new js.Callback.once((p){
+              // continue creating and loading the file
+              createAndLoadFile(new SimpleOAuth2(js.context['gapi']['auth']['getToken']()['access_token']));
+              // TODO there should also be a handler for the case where the immediate auth doesn't
+              // go through and does the pop-up authentication
+          }));
+        }));
+      } else {
+        // 2:
+        // create auth object
+        var dartAuth = new GoogleOAuth2(
+          '1066816720974.apps.googleusercontent.com',
+          ['https://www.googleapis.com/auth/drive.install',
+           'https://www.googleapis.com/auth/drive.file',
+           'openid'],
+          // TODO this calls login but doesn't let the exception through
+          // so we have to do it with login calls below
+          autoLogin: false
+        );
+        // function to install gapi.auth.getToken and continue with createAndLoadFile
+        var onTokenLoad = (Token t) {
+          // overwrite gapi.auth.getToken with a function that
+          // returns an object with valid data in access_token
+          js.context['gapi']['auth'] = js.map({
+            'getToken':
+              new js.Callback.many(() =>
+                js.map({
+                  'access_token': t.data
+                }))
+          });
+          // continue with creating a drive file and loading realtime object
+          createAndLoadFile(dartAuth);
+        };
+        // try silent login
+        dartAuth.login(immediate: true).then(onTokenLoad).catchError((obj) {
+          // if no immediate auth, show window to get auth
+          dartAuth.login(immediate: false).then(onTokenLoad).catchError((obj) {
+            print('authentication failue');
+          });
+        });
+      }
+    }));
   }
 }
 
