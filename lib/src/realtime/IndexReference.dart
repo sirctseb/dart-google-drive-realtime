61ce9597e6c42ef263889bb000840871daa966b2
Christopher Best <bestchris@gmail.com>
2015-05-01 14:20:20 -0400

    updated IndexReference

--- lib/src/realtime/IndexReference.dart
+++ lib/src/realtime/IndexReference.dart
@@ -14,6 +14,13 @@
 
 part of realtime_data_model;
 
+// TODO this is a weird construction to get the same syntax as js lib
+class DeleteModeType {
+  String get SHIFT_AFTER_DELETE => realtime['IndexReference']['DeleteMode']['SHIFT_AFTER_DELETE'];
+  String get SHIFT_BEFORE_DELETE => realtime['IndexReference']['DeleteMode']['SHIFT_BEFORE_DELETE'];
+  String get SHIFT_TO_INVALID => realtime['IndexReference']['DeleteMode']['SHIFT_TO_INVALID'];
+}
+
 class IndexReference extends CollaborativeObject {
   SubscribeStreamProvider<ReferenceShiftedEvent> _onReferenceShifted;
 
@@ -21,6 +28,9 @@ class IndexReference extends CollaborativeObject {
     _onReferenceShifted = _getStreamProviderFor(EventType.REFERENCE_SHIFTED, ReferenceShiftedEvent._cast);
   }
 
+  static DeleteModeType DeleteMode;
+
+  String get deleteMode => $unsafe.callMethod('deleteMode');
   bool get canBeDeleted => $unsafe['canBeDeleted'];
   int get index => $unsafe['index'];
   set index(int i) => $unsafe['index'] = i;
